12889979f7e3652370c52714afbed993125c68c0
root <root@alainkassabian.com>
2021-06-15 16:11:45 +0000

    update image and copy

--- themes/ajk/footer.php
+++ themes/ajk/footer.php
@@ -7,7 +7,7 @@
 			<div class="row social-icons">
 				<div class="col d-flex justify-content-center">
 					<ul class="d-flex">
-						<li><a href="https://github.com/akassabian">
+						<li><a href="https://github.com/alaindwight">
 							<svg class="social-github" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 							viewBox="0 0 841.89 841.89" enable-background="new 0 0 841.89 841.89" xml:space="preserve">
 								<path fill-rule="evenodd" clip-rule="evenodd" fill="#0F0C0D" d="M422.399,3C203.499,3,25.999,180.477,25.999,399.425
@@ -22,10 +22,10 @@
 									C818.809,180.477,641.333,3,422.399,3"/>
 							</svg>
 						</a></li>
-						<li><a href="https://www.linkedin.com/in/alain-kassabian/">
+						<li><a href="https://www.linkedin.com/in/alain-dwight/">
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
 						</a></li>
-						<li><a href="mailto:alainkassabian@gmail.com">
+						<li><a href="mailto:alaindwight@gmail.com">
 							<svg class="social-email" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z"/></svg>
 						</a></li>
 					</ul>
